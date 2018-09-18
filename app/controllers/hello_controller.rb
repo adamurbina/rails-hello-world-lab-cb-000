d168b2cc4fd2d98173be67b4822e529dfe48b706
Adam Urbina <adamurbina@gmail.com>
2018-09-18 13:27:56 +0000

    automatically backed up by learn

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,5 @@
+class HelloController < ApplicationController
+  def hello
+    render 'hello_page'
+  end
+end
--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ApplicationController
-  def hello
-    render 'hello_page'
-  end
-end
